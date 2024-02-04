bd8ed54c362de714f1221746ef9c8fe9cd3993c1
Leonid Znamenok <respublica@altlinux.org>
2024-02-04 21:26:52 +0300

    Added base functions

---
+++ homework/utils.py
@@ -0,0 +1,23 @@
+import math
+
+
+def getFiboClassic(n):
+    fibo1 = 1
+    fibo2 = 1
+    for i in range(2, n):
+        fibo3 = fibo1 + fibo2
+        fibo1 = fibo2
+        fibo2 = fibo3
+
+    return fibo2
+
+
+def getFiboRecursive(n):
+    if n in (1, 2):
+        return 1
+    return getFiboRecursive(n - 1) + getFiboRecursive(n - 2)
+
+
+def getFiboMoivreBinet(n):
+    return round(((((1+math.sqrt(5))/2)**n)
+                 - (((1-math.sqrt(5))/2)**n))/math.sqrt(5))
